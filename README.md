e8fae1119b54341dc0a8e4d395c66f66fb537875
jrgauntjr <joseph.r.gaunt@gmail.com>
2025-03-11 15:20:40 -0600

    first comment

---
+++ README.md
@@ -0,0 +1,9 @@
+calculators
+=====
+
+An OTP library
+
+Build
+-----
+
+    $ rebar3 compile
---
+++ src/calculators.erl
@@ -0,0 +1,13 @@
+-module(calculators).
+
+-export([fib/1, calc/1]).
+
+
+
+-spec fib({integer(), integer()}) -> integer().
+fib({Prev, Curr}) -> TODO.
+
+
+
+-spec calc({integer(), string(), integer()}) -> integer().
+calc({NUM1, SIGN, NUM2}) -> TODO.
