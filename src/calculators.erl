92673ddcce5311386838b83467d443adb6e96e08
jrgauntjr <joseph.r.gaunt@gmail.com>
2025-03-13 15:29:04 -0600

    Wrote out the basic calculator function

--- src/calculators.erl
+++ src/calculators.erl
@@ -1,13 +1,29 @@
 -module(calculators).
 
--export([fib/1, calc/1]).
+-export([fib/1, calc/3]).
 
 
 
 -spec fib({integer(), integer()}) -> integer().
-fib({Prev, Curr}) -> TODO.
+fib({Prev, Curr}) -> 1.
 
 
 
--spec calc({integer(), string(), integer()}) -> integer().
-calc({NUM1, SIGN, NUM2}) -> TODO.
+-spec calc(integer(), atom(), integer()) -> number().
+calc(P1, Op, P2) when Op == '+' -> P1 + P2;
+calc(N1, Op, N2) when Op == '-' -> N1 - N2;
+calc(M1, Op, M2) when Op == '*' -> M1 * M2;
+calc(D1, Op, D2) when Op == '/' -> D1 div D2;  
+calc(_, _, _) -> erlang:error(badarg).  
+
+
+
+-ifdef(EUNIT).
+-include_lib("eunit/include/eunit.hrl").
+
+calc_test_() ->
+    [
+    ?_assertEqual(2, calc(1,'+',1))
+    ].
+
+-endif.
