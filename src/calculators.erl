30d7610f7b30ba4f73931ef281d80a84aae307f6
jrgauntjr <joseph.r.gaunt@gmail.com>
2025-03-20 13:51:26 -0600

    Made minor adjustments

--- src/calculators.erl
+++ src/calculators.erl
@@ -2,14 +2,10 @@
 
 -export([fib/1, calc/3, square_list/1]).
 
-
-
 -spec fib(integer()) -> integer().
 fib(0) -> 0;
 fib(1) -> 1;
 fib(N) when N > 1 -> fib(N-1) + fib(N-2).
-
-
 
 -spec calc(integer(), atom(), integer()) -> number().
 calc(P1, Op, P2) when Op == '+' -> P1 + P2;
@@ -17,7 +13,6 @@ calc(N1, Op, N2) when Op == '-' -> N1 - N2;
 calc(M1, Op, M2) when Op == '*' -> M1 * M2;
 calc(D1, Op, D2) when Op == '/' -> D1 div D2;  
 calc(_, _, _) -> erlang:error(badarg).  
-
 
 -spec square_list(list(integer())) -> list(integer()).
 square_list(L) -> lists:map(fun(X) -> X * X end, L).
